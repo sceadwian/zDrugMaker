474d1ece81ab40f5aa3996806b72ef89e8cc69e0
sceadwian <leo.silenieks+accounts@alumni.utoronto.ca>
2023-06-01 23:49:14 -0400

    additional functionality for outputs

--- zToggl.py
+++ zToggl.py
@@ -36,14 +36,21 @@ def parse_csv(input_file, output_file):
             csv_writer = csv.writer(out_file)
             csv_writer.writerow(['Date', 'Start time', 'End time', 'Total hours'])
             for date, time_ranges in data.items():
-                min_start_time = min(time_ranges, key=lambda x: x[0])[0]
-                max_end_time = max(time_ranges, key=lambda x: x[1])[1]
-                total_hours = sum((end_time - start_time).total_seconds() / 3600 for start_time, end_time in time_ranges)
-                csv_writer.writerow([date, min_start_time.time(), max_end_time.time(), total_hours])
+                time_ranges.sort(key=lambda x: x[0])
+                start_time, end_time = time_ranges[0]
+                for next_start_time, next_end_time in time_ranges[1:]:
+                    if next_start_time - end_time > timedelta(hours=1):
+                        total_hours = (end_time - start_time).total_seconds() / 3600
+                        csv_writer.writerow([date, start_time.time(), end_time.time(), f'{total_hours:.2f}'])
+                        print(f'Date: {date}, Start time: {start_time.time()}, End time: {end_time.time()}, Total hours: {total_hours:.2f}')
+                        start_time = next_start_time
+                    end_time = max(end_time, next_end_time)
+                total_hours = (end_time - start_time).total_seconds() / 3600
+                csv_writer.writerow([date, start_time.time(), end_time.time(), f'{total_hours:.2f}'])
                 
                 print(f'Date: {date}')
-                print(f'Start time: {min_start_time.time()}')
-                print(f'End time: {max_end_time.time()}')
+                print(f'Start time: {start_time.time()}')
+                print(f'End time: {end_time.time()}')
                 print(f'Total hours: {total_hours:.2f}')
                 print("Description breakdown:")
                 for (client, bill, study), duration in description_data[date].items():
