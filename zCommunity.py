ec53321b93ac250280674ebb2f3293ee2edf0df4
sceadwian <leo.silenieks+accounts@alumni.utoronto.ca>
2023-06-16 23:42:36 -0400

    Now with soldiers

--- zCommunity.py
+++ zCommunity.py
@@ -37,8 +37,8 @@ COLORS = {
 }
 
 # Define the map
-map_width = 96  # Increased width
-map_height = 64  # Reduced height
+map_width = 96
+map_height = 64
 map = [[GROUND]*map_width for _ in range(map_height)]
 
 # Add border to the map
@@ -56,8 +56,35 @@ for community in communities:
     x, y = random.randint(1, map_height), random.randint(1, map_width)
     map[x][y] = community
 
+# Soldier class
+class Soldier:
+    def __init__(self, x, y, community):
+        self.x = x
+        self.y = y
+        self.community = community
+
+    def move(self):
+        direction = random.choice(['up', 'down', 'left', 'right'])
+        if direction == 'up':
+            self.x -= 1
+        elif direction == 'down':
+            self.x += 1
+        elif direction == 'left':
+            self.y -= 1
+        elif direction == 'right':
+            self.y += 1
+        self.x = max(1, min(self.x, map_height))
+        self.y = max(1, min(self.y, map_width))
+
+    def attack(self):
+        if map[self.x][self.y] in communities and map[self.x][self.y] != self.community:
+            map[self.x][self.y] = GROUND
+
+# Initialize soldiers
+soldiers = {community: [Soldier(random.randint(1, map_height), random.randint(1, map_width), community)] for community in communities}
+
 # Function to get neighboring cells within a given radius
-def get_neighbors(x, y, radius=1):  # default radius set to 1 for adjacent cells
+def get_neighbors(x, y, radius=1):
     neighbors = []
     for i in range(x-radius, x+radius+1):
         for j in range(y-radius, y+radius+1):
@@ -74,72 +101,71 @@ def expand_community(community):
         VEGETATION: 25,
     }
     
-    # Find the current community locations
     locations = [(i, j) for i in range(1, map_height+1) for j in range(1, map_width+1) if map[i][j] == community]
     
-    # Try to expand the community from each location
     for location in locations:
         x, y = location
-        neighbors = get_neighbors(x, y, 1)  # get neighboring cells within radius of 1
+        neighbors = get_neighbors(x, y, 1)
         for neighbor in neighbors:
             nx, ny = neighbor
             if map[nx][ny] in elements:
                 map[nx][ny] = community if random.randint(1, 10) <= growth_rate[map[nx][ny]] else map[nx][ny]
 
-    # Spawn a new settlement probabilistically every 10 years, influenced by community growth rate
     if year % 10 == 0 and random.random() < 0.2 * GROWTH_RATE[community]:
         while True:
             x, y = random.randint(1, map_height), random.randint(1, map_width)
-            # Make sure the new settlement is spawned next to an existing one
             if map[x][y] not in communities and any(map[nx][ny] == community for nx, ny in get_neighbors(x, y)):
                 map[x][y] = community
                 break
 
 def decay_community(community):
-    # Find all community cells
     community_cells = [(i, j) for i in range(1, map_height+1) for j in range(1, map_width+1) if map[i][j] == community]
-
-    # If there is more than one community cell, randomly select one to decay
     if len(community_cells) > 1:
         x, y = random.choice(community_cells)
-        map[x][y] = GROUND  # revert the cell to its original state
+        map[x][y] = GROUND
 
-
-# Function to clear console
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Function to count community size
 def count_community(community):
     return sum(row.count(community) for row in map)
 
-# Run the simulation
 years = 500000
 for year in range(years):
     try:
         for community in communities:
             expand_community(community)
-
-            # Occasionally remove a community from a tile
             if year % 100 == 0:
                 decay_community(community)
+            for soldier in soldiers[community]:
+                soldier.move()
+                soldier.attack()
 
-        # Only update the console every 10 time steps
+        if year % 50 == 0:
+            for community in communities:
+                if random.random() < count_community(community) / (map_width * map_height):
+                    soldiers[community].append(Soldier(random.randint(1, map_height), random.randint(1, map_width), community))
+
         if year % 10 == 0:
-            # Clear the console and print the current state of the map
             clear()
             print(f"Year: {year}")
-            for row in map:
-                print(''.join(COLORS[cell] + cell for cell in row))
+            for i, row in enumerate(map):
+                row_string = ''
+                for j, cell in enumerate(row):
+                    for community in communities:
+                        if any(soldier.x == i and soldier.y == j for soldier in soldiers[community]):
+                            row_string += COLORS[community] + 'S'
+                            break
+                    else:
+                        row_string += COLORS[cell] + cell
+                print(row_string)
 
-            # Print community sizes
             print("Community sizes:")
             print(f"{COMMUNITY_A}: {count_community(COMMUNITY_A)}")
             print(f"{COMMUNITY_B}: {count_community(COMMUNITY_B)}")
             print(f"{COMMUNITY_C}: {count_community(COMMUNITY_C)}")
             print(f"{COMMUNITY_D}: {count_community(COMMUNITY_D)}")
 
-        # Pause between each turn
         time.sleep(0.000000002)
     except KeyboardInterrupt:
         print(f"\nSimulation stopped after {year+1} years.")
